ec040d699375f158fd5bbcf140dabc95785115a5
Shy Ruparel <Shy@users.noreply.github.com>
2020-05-13 10:08:06 -0400

    Set ENV vars to all caps

--- _data/contentful-brand.js
+++ _data/contentful-brand.js
@@ -2,9 +2,9 @@ const contentful = require("contentful");
 
 const client = contentful.createClient({
     // This is the space ID. A space is like a project folder in Contentful terms
-    space: process.env.CTFL_space,
+    space: process.env.CTFL_SPACE,
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: process.env.CTFL_accessToken
+    accessToken: process.env.CTFL_ACCESSTOKEN
 });
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 
